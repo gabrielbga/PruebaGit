3101594742b3b4263e6ab7ae8f45342da9e2a945
Belmont <gabrielbga92@gmail.com>
2020-07-24 11:54:28 -0500

    Inicia el trabajo con Angular y Spring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Prueba con Ramas y Fusiones en Git';
+  title = 'Continuando con Angular!!';
+  curso = 'Angular & Spring';
+  profesor = 'Belmont';
 }
