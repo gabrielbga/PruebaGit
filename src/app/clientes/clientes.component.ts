cd2b8ed33e6fbf045cbb3f1fffa972c6572d5b1b
Belmont <gabrielbga92@gmail.com>
2020-07-27 17:30:01 -0500

    Se configuro e integro el bootstrap de forma local en la app

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+
 
 @Component({
   selector: 'app-clientes',
@@ -7,17 +9,14 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ClienteService: ClienteService) { }
 
-  clientes: Cliente[] = [
-    { id: 1, nombre: 'Gabriel', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
-    { id: 2, nombre: 'Malthael', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
-    { id: 3, nombre: 'Alucard', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
-    { id: 4, nombre: 'Gabriela', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
-    { id: 5, nombre: 'Ana', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
-    { id: 6, nombre: 'Valen', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'}
-  ];
+  clientes: Cliente[];
+
   ngOnInit(): void {
+    this.ClienteService.getClientes().subscribe(
+      clientes => this.clientes = clientes
+    );
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombre: 'Gabriel', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
+    { id: 2, nombre: 'Malthael', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
+    { id: 3, nombre: 'Alucard', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
+    { id: 4, nombre: 'Gabriela', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
+    { id: 5, nombre: 'Ana', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'},
+    { id: 6, nombre: 'Valen', apellido: 'Belmont', email: 'belmont@belmont.com', createAt: '2020-12-11'}
+  ];
